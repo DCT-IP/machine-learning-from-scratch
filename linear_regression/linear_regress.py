fb61029fa2e30d207c2706d7b06fe6eff6d5d19c
DCT <dheemanchaitanyathakur@gmail.com>
2026-01-12 01:35:42 +0530

    added comments for clarity

--- linear_regression/linear_regress.py
+++ linear_regression/linear_regress.py
@@ -4,8 +4,10 @@ Y_train = np.array([4,7,10,13,16]) #assume the function to be y = 3x + 1
 
 X_train = np.column_stack((np.stack(np.ones(len(X_train))),X_train))
 w = np.linalg.pinv(X_train) @ Y_train #least squares soln
+#pinv was used to pseudo inverse 
 print(w) #should be two
-
+#here we perfomed the operation of changing X-train as 
+# y= mx + b is hard to express in terms of 2-3 matrices it is better and efficient to have a nx2 matrix expressing the set of equations and far easier to calculate
 Y_test = X_train @ w
 print("\nPredictions from training model",Y_test) #seeing error margin
 residual = Y_train - Y_train  #E = ||y - Ax||
