2033f43730d9c0ea74d9e0c4cf458f0dd8d8b15b
DCT <dheemanchaitanyathakur@gmail.com>
2026-01-12 23:33:39 +0530

    added noise and rewrote the code to make it better

--- linear_regression/linear_regress.py
+++ linear_regression/linear_regress.py
@@ -1,15 +1,24 @@
 import numpy as np
-X_train = np.array([1,2,3,4,5])
-Y_train = np.array([4,7,10,13,16]) #assume the function to be y = 3x + 1
+X_train = np.array([1,1.5,2,3,3.8,4,5])
+Y_train = np.array([3,2,5,7,8.9,9,11]) #assuming the equation to be y = 2x+1
+#above is our target
 
-X_train = np.column_stack((np.stack(np.ones(len(X_train))),X_train))
-w = np.linalg.pinv(X_train) @ Y_train #least squares soln
-#pinv was used to pseudo inverse 
-print(w) #should be two
-#here we perfomed the operation of changing X-train as 
-# y= mx + b is hard to express in terms of 2-3 matrices it is better and efficient to have a nx2 matrix expressing the set of equations and far easier to calculate
-Y_test = X_train @ w
-print("\nPredictions from training model",Y_test) #seeing error margin
-residual = Y_train - Y_train  #E = ||y - Ax||
-#to check 
-print("\nOrthogonality check:- ", X_train.T @ residual)
+X = np.array(np.column_stack((np.stack(np.ones(len(X_train))),X_train)))  #basis
+#this allows to write y = b + mx in the form of b = Ax
+#the matrix of X is [1 xi]        system observed while solving a linear sys
+
+w = np.linalg.pinv(X)@Y_train          #coordinates 
+#solving for least squares solution     - here x will store our [bias and weight]
+
+print("bias, weight:",w) # to check the values
+
+#to check orthognality as X^T.e = 0
+residual = Y_train - X@w
+print("orthogonality check", X.T@residual)
+print("residual norm: ",np.linalg.norm(residual))
+#to see predictability
+X_test = np.array([6,7,8]);
+X_test= np.array(np.column_stack((np.stack(np.ones(len(X_test))),X_test)))
+Y_test = X_test@w
+print("test values: ",Y_test)
+# added noise to understand its effects on training the model
