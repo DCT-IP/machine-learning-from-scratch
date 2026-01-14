fcc97e3aeb14bd6f8b00669f1f0648f7d6d47de1
DCT <dheemanchaitanyathakur@gmail.com>
2026-01-14 23:52:55 +0530

    changed the program to have functions

--- linear_regression/linear_regress.py
+++ linear_regression/linear_regress.py
@@ -1,24 +1,39 @@
 import numpy as np
-X_train = np.array([1,1.5,2,3,3.8,4,5])
-Y_train = np.array([3,2,5,7,8.9,9,11]) #assuming the equation to be y = 2x+1
-#above is our target
+def Design_mat(X):
+    # for creation of design matrix 
+    #changes matrix to suit the form of linear equations ' Ax = b ' better
+    X = np.array(np.column_stack((np.stack(np.ones(len(X))),X)))
+    return X
 
-X = np.array(np.column_stack((np.stack(np.ones(len(X_train))),X_train)))  #basis
-#this allows to write y = b + mx in the form of b = Ax
-#the matrix of X is [1 xi]        system observed while solving a linear sys
+def Normal_Eqn(X,Y):
+    #using Least Squares to solve
+    w = np.linalg.pinv(X)@Y
+    return w
 
-w = np.linalg.pinv(X)@Y_train          #coordinates 
-#solving for least squares solution     - here x will store our [bias and weight]
+def residual(x,y,w):
+    #calculates residuals
+    return y-x@w
 
-print("bias, weight:",w) # to check the values
+def OrthoCheck(x,res):
+    #should be 0 or something equivalent
+    return x.T@res
 
-#to check orthognality as X^T.e = 0
-residual = Y_train - X@w
-print("orthogonality check", X.T@residual)
-print("residual norm: ",np.linalg.norm(residual))
-#to see predictability
-X_test = np.array([6,7,8]);
-X_test= np.array(np.column_stack((np.stack(np.ones(len(X_test))),X_test)))
-Y_test = X_test@w
-print("test values: ",Y_test)
-# added noise to understand its effects on training the model
+def predict(x,w):
+    #to test out the data 
+    return x@w
+
+def main():
+    X_train = np.array([1,1.5,2,3,3.8,4,5])
+    Y_train = np.array([3,2,5,7,8.9,9,11])
+    X_test = np.array([6,7,8])
+    X_test = Design_mat(X_test)
+    X = Design_mat(X_train)
+    w = Normal_Eqn(X,Y_train)
+    Err = residual(X,Y_train,w)
+    print("Norm for the residual: ", np.linalg.norm(Err))
+    print("On Orthogonality Check:-", OrthoCheck(X,Err))
+    print("On trying out the equation with ", w, " as bias and weight and data as \n",X_test,"\n: ",predict(X_test,w))
+
+if __name__ == "__main__":
+    main()
+
